3ab3488aac76ddab3d21357476d62f579531bfd0
Michael Coombs <mcoombs@ycp.edu>
2012-04-25 10:18:44 -0400

    Updating gui

--- src/edu/ycp/cs320/project/gui/GuiMain.java
+++ src/edu/ycp/cs320/project/gui/GuiMain.java
@@ -12,15 +12,21 @@ import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 
 import edu.ycp.cs320.project.Book;
+import edu.ycp.cs320.project.FormatType;
 import edu.ycp.cs320.project.Periodical;
 import edu.ycp.cs320.project.SourceType;
 import edu.ycp.cs320.project.Website;
 import edu.ycp.cs320.project.controller.BookController;
+import edu.ycp.cs320.project.controller.PeriodicalController;
 import edu.ycp.cs320.project.controller.WebsiteController;
 import edu.ycp.cs320.project.gui.BookView;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
+import java.awt.Dimension;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 
 public class GuiMain extends JFrame {
 	/**
@@ -41,10 +47,12 @@ public class GuiMain extends JFrame {
 	 * Create the frame.
 	 */
 	public GuiMain() {
+		setPreferredSize(new Dimension(850, 700));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		getContentPane().setLayout(null);
 		
 		sourceViewContainerPanel = new JPanel();
-		sourceViewContainerPanel.setBounds(38, 58, 745, 343);
+		sourceViewContainerPanel.setBounds(38, 58, 447, 343);
 		getContentPane().add(sourceViewContainerPanel);
 		sourceViewContainerPanel.setLayout(new CardLayout(0, 0));
 		
@@ -58,9 +66,41 @@ public class GuiMain extends JFrame {
 		sourceTypeComboBox.setBounds(38, 27, 224, 20);
 		getContentPane().add(sourceTypeComboBox);
 		
+		JTextArea outputTextArea = new JTextArea();
+		outputTextArea.setBounds(38, 475, 768, 204);
+		getContentPane().add(outputTextArea);
+		
+		JButton generateButton = new JButton("Generate");
+		generateButton.setName("GenerateButton");
+		generateButton.setBounds(316, 436, 89, 23);
+		getContentPane().add(generateButton);
+		
+		JLabel typeLabel = new JLabel("Citation Type:");
+		typeLabel.setBounds(38, 11, 75, 14);
+		getContentPane().add(typeLabel);
+		
+		JLabel formatLabel = new JLabel("Citation Format:");
+		formatLabel.setBounds(38, 412, 96, 14);
+		getContentPane().add(formatLabel);
+		
+		JComboBox<FormatType> formatComboBox = new JComboBox<FormatType>();
+		formatComboBox.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				formatTypeChanged();
+			}
+		});
+		formatComboBox.setModel(new DefaultComboBoxModel<FormatType>(FormatType.values()));
+		formatComboBox.setBounds(38, 437, 224, 20);
+		getContentPane().add(formatComboBox);
+		
 		
 	}
 	
+	protected void formatTypeChanged() {
+		// TODO Auto-generated method stub
+		
+	}
+
 	public void createSourceViews() {
 		
 		this.book = new Book();
@@ -83,6 +123,9 @@ public class GuiMain extends JFrame {
 		this.periodicalView = new PeriodicalView();
 		periodicalView.setModel(periodical);
 		PeriodicalController periodicalController = new PeriodicalController();
+		periodicalView.setController(periodicalController);
+		periodicalController.setModel(periodical);
+		sourceViewContainerPanel.add(periodicalView, SourceType.PERIODICAL.toString());
 		
 		CardLayout cardLayout = (CardLayout) sourceViewContainerPanel.getLayout();
 		cardLayout.show(sourceViewContainerPanel, SourceType.BOOK.toString());
